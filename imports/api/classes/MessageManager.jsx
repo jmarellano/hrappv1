4a76cafefacd02c9d9cc17f980184264884c6479
Patrick Reyes <watafungya@gmail.com>
2018-07-16 10:14:27 -0400

    follow up
    Signed-off-by: Patrick Reyes <watafungya@gmail.com>

--- imports/api/classes/MessageManager.jsx
+++ imports/api/classes/MessageManager.jsx
@@ -30,7 +30,6 @@ export default class MessageManager {
             UserDB.update({ _id: userId }, { $set: { 'profile.importing': VALUE.TRUE } });
             let spawn = child_process.spawn;
             const ls = spawn('java', [ '-jar', '/data/JavaPST.jar', file ]);
-            console.log(wakanda);
             ls.stdout.on('data', (data) => {
                 //console.log(`stdout: ${data}`);
             });
